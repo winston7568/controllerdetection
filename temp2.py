02af9fe89f1b95cecd99594593efdf8e34866001
winston7568 <56356383+winston7568@users.noreply.github.com>
2022-09-29 19:03:17 -0400

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -35,36 +35,9 @@ target_ip_entry.focus()
 # port_entry = ttk.Entry(frame, textvariable=port)
 # port_entry.grid(column=1, row=1, **options)
 # port_entry.focus()
-
-# submit  button
-
-
-def submit_button_clicked():
-    """  Handle submit button click event
-    """
-    try:
-        t = str(target_ip.get())
-        # po = str(port.get())
-        result = f'The target ip address {t} '  #and the port number is  {p} '
-        result_label.config(text=result)
-    except ValueError as error:
-        showerror(title='Error', message=error)
-
-
-submit_button = ttk.Button(frame, text='submit')
-submit_button.grid(column=2, row=1, sticky='W', **options)
-submit_button.configure(command=submit_button_clicked)
-
-# result label
-result_label = ttk.Label(frame)
-result_label.grid(row=3, columnspan=3, **options)
-
-# add padding to the frame and show it
-frame.grid(padx=10, pady=10)
-
 #check the controller 
 
-def Checkcontroller(t,po):
+def checkController(t):
  # defaultGuiPorts = {"Floodlight & OpenDayLight": 8080, "OpenDayLight (DLUX Standalone)": 9000, "OpenDayLight (DLUX w/t Karaf) & ONOS": 8181}
  defaultGuiURLs = {"Floodlight": "/ui/index.html", "OpenDayLight (DLUX)": "/dlux/index.html", "OpenDayLight (Hydrogen)": "/index.html", "ONOS": "/onos/ui/login.html"}
  guiIdentifiers = {}
@@ -95,6 +68,34 @@ def Checkcontroller(t,po):
                 error_label.config(text=error_message)
  print("")
            
+# submit  button
+
+
+def submit_button_clicked():
+    """  Handle submit button click event
+    """
+    try:
+        t = str(target_ip.get())
+        # po = str(port.get())
+        c = checkController(t)
+        result = f'The target ip address {t} , {c} '  #and the port number is  {p} '
+        result_label.config(text=result)
+    except ValueError as error:
+        showerror(title='Error', message=error)
+
+
+submit_button = ttk.Button(frame, text='submit')
+submit_button.grid(column=2, row=1, sticky='W', **options)
+submit_button.configure(command=submit_button_clicked)
+
+# result label
+result_label = ttk.Label(frame)
+result_label.grid(row=3, columnspan=3, **options)
+
+# add padding to the frame and show it
+frame.grid(padx=10, pady=10)
+
+
 
 
 
